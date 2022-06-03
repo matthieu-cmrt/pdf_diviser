1caab248816c32e6ec9ce17b696d040522608495
matthieu-cmrt <matthieu.camart@gmail.com>
2022-06-03 14:39:48 +0200

    PDF_Diviser: Added test

--- pdf_diviser.py
+++ pdf_diviser.py
@@ -1,14 +1,12 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import os
 
-from requests import patch
-
 # for each pdf in src folder, path = src/file.pdf and name = file
 for file in os.listdir("src"):
     # if file is a pdf
     if not file.endswith(".pdf"):
         continue
-    path = "src/" + file
+    path = "./src/" + file
     name = file[:-4]    
     inputpdf = PdfFileReader(open   (path, "rb"))
 
